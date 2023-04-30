7484688b83c1f0395e84852bf787c0f0776b3c11
Danilo Silva <badkill82@gmail.com>
2023-04-30 22:02:42 +0200

    chore: improve module documentation

--- lib/ecto_command/command.ex
+++ lib/ecto_command/command.ex
@@ -1,5 +1,39 @@
 defmodule EctoCommand.Command do
-  @moduledoc false
+  @moduledoc """
+  The `EctoCommand.Command` module provides a DSL for defining command schemas.
+  It is used by `use EctoCommand.Command` in your command modules.
+
+  ### Example
+      defmodule MyApp.Commands.CreatePost do
+        use EctoCommand.Command
+        alias MyApp.PostRepository
+
+        command do
+          param :title, :string, required: true, length: [min: 3, max: 255]
+          param :body, :string, required: true, length: [min: 3]
+
+          internal :slug, :string
+          internal :author, :string
+        end
+
+        def execute(%__MODULE__{} = command) do
+          ...
+          :ok
+        end
+
+        def fill(:slug, _changeset, %{"title" => title}, _metadata) do
+          Slug.slufigy(title)
+        end
+
+        def fill(:author, _changeset, _params, %{"triggered_by" => triggered_by}) do
+          triggered_by
+        end
+
+        def fill(:author, changeset, _params, _metadata) do
+          Ecto.Changeset.add_error(changeset, :triggered_by, "triggered_by metadata info is missing")
+        end
+      end
+  """
 
   alias EctoCommand.Command
   alias EctoCommand.Middleware.Pipeline
@@ -35,7 +69,9 @@ defmodule EctoCommand.Command do
       Module.register_attribute(__MODULE__, :command_fields, accumulate: true)
       Module.register_attribute(__MODULE__, :middlewares, accumulate: true)
 
-      @doc false
+      @doc """
+      The `fill/4` function takes four arguments: the name of the field, the current temporary changeset, the parameters received from external sources, and additional metadata. You can choose to return the value that will populate the field or the updated changeset. Both options are acceptable, but returning the changeset is particularly useful if you want to add errors to it.
+      """
       def fill(_, changeset, _params, _metadata), do: changeset
 
       defoverridable fill: 4
@@ -45,12 +81,25 @@ defmodule EctoCommand.Command do
   @doc false
   defmacro __before_compile__(_env) do
     quote unquote: false do
-      def new(attributes, metadata \\ %{}) do
-        attributes
+      # The new/2 function creates a new command struct with the given params and metadata
+      # in case of invalid data it returns an error changeset.
+      # ## Examples
+      #   new(%{name: "John", age: 28})
+      #   new(%{name: "John", age: 28}, %{user_id: 1})
+      @spec new(params :: map, metadata :: map) :: {:ok, struct()} | {:error, Ecto.Changeset.t()}
+      def new(%{} = params, metadata \\ %{}) do
+        params
         |> changeset(metadata)
         |> apply_action(:insert)
       end
 
+
+      # The changeset/2 function creates a new changeset with the given params and validates it against the given schema.
+      # It also fills the internal fields with the given metadata.
+      # ## Examples
+      #    changeset(%{name: "John", age: 28})
+      #    changeset(%{name: "John", age: 28}, %{user_id: 1})
+      @spec changeset(params :: map, metadata :: map) :: Ecto.Changeset.t()
       def changeset(%{} = params, metadata \\ %{}) do
         params = Command.trim_fields(params, @trim_fields)
 
@@ -61,6 +110,7 @@ defmodule EctoCommand.Command do
         |> __fill_internal_fields(metadata)
       end
 
+      @spec execute(params :: map, metadata :: map) :: any() | {:error, Ecto.Changeset.t()}
       def execute(%{} = params, metadata \\ %{}) when is_map(params) do
         Command.execute(%Pipeline{
           params: params,
@@ -88,9 +138,18 @@ defmodule EctoCommand.Command do
     end
   end
 
+  @doc false
   def parse_block({:__block__, context, block}), do: {:__block__, context, Enum.map(block, &parse_block/1)}
   def parse_block(block), do: block
 
+  @doc """
+  The command/1 macro defines a command schema with the given block.
+  ## Examples
+       command do
+         param :name, :string
+         param :age, :integer
+       end
+  """
   defmacro command(do: block) do
     prelude =
       quote do
@@ -162,6 +221,14 @@ defmodule EctoCommand.Command do
     end
   end
 
+  @doc """
+  Defines a command internal field.  \n
+  These fields will be ignored during the "cast process".
+  Instead, you need to define a public `fill/4` function to populate them. The `fill/4` function takes four arguments: the name of the field, the current temporary changeset, the parameters received from external sources, and additional metadata. You can choose to return the value that will populate the field or the updated changeset. Both options are acceptable, but returning the changeset is particularly useful if you want to add errors to it.
+  ## Examples
+       internal :slug, :string
+       internal :author, :string
+  """
   defmacro internal(name, type \\ :string, opts \\ []) do
     quote do
       opts = unquote(opts)
@@ -177,6 +244,17 @@ defmodule EctoCommand.Command do
     end
   end
 
+  @doc """
+  Defines a command parameter field.  \n
+  The `param` macro is based on the `field` macro of [Ecto.Schema](https://hexdocs.pm/ecto/Ecto.Schema.html) and defines a field in the schema with a given name and type. You can pass all the options supported by the `field` macro. Afterwards, each defined `param` is cast with the "external" data received.
+  In addition to those options, the `param` macro accepts a set of other options that indicate how external data for that field should be validated.
+  These options are applied to the intermediate Changeset created in order to validate data.
+  These options are mapped into `validate_*` methods of the [Ecto.Changeset](https://hexdocs.pm/ecto/Ecto.Changeset.html).
+  ## Examples
+
+      param :title, :string, required: true, length: [min: 3, max: 255]
+      param :body, :string, required: true, length: [min: 3]
+  """
   defmacro param(name, type, opts \\ []) do
     quote do
       opts = unquote(opts)
@@ -214,12 +292,16 @@ defmodule EctoCommand.Command do
     end
   end
 
+  @doc false
+  @spec trim_fields(map(), [atom()]) :: map()
   def trim_fields(params, trim_fields) do
     Enum.reduce(trim_fields, params, fn field, params ->
       Map.update(params, field, nil, &String.trim/1)
     end)
   end
 
+  @doc false
+  @spec execute(Pipeline.t()) :: any() | {:error, Ecto.Changeset.t()}
   def execute(%Pipeline{} = pipeline) do
     pipeline
     |> instantiate_command()
